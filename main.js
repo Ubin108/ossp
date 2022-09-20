fc8242c93836d74938f220577f5b33830b09a111
김유빈 <ybin0108@gmail.com>
2022-09-20 18:54:15 +0900

    Implemented sub

--- main.js
+++ main.js
@@ -11,10 +11,14 @@ function mod(a,b) {
 }
 
 function sub(a,b){
-
+    return a-b;
 }
 
 function mul(a,b){
+<<<<<<< HEAD
     return a*b
+=======
+
+>>>>>>> 7f50974 (Implemented sub)
 }
 console.log(div(10,5))
